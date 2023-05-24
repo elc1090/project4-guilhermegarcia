6cea841a9416281d7b3ee00933b721fb8e77d7c0
Guilherme <gmgarcia@inf.ufsm.br>
2023-05-24 19:45:40 -0300

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Inertia\Response;
+use Inertia\Inertia;
+
+class PostController extends Controller
+{
+    public function index() : Response
+    {
+        return Inertia::render('Posts/Index', [
+            'posts' => Post::with('user:id,name')->latest()->get(),
+        ]);
+    }
+
+    public function store(Request $request) : RedirectResponse
+    {
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $request->user()->posts()->create($validated);
+
+        return redirect(route('posts.index'));
+    }
+}
---
+++ resources/js/Components/Post.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps(['post']);
+</script>
+
+<template>
+    <div class="p-6 flex space-x-2">
+        <div class="flex-1">
+            <div class="flex justify-between items-center">
+                <div>
+                    <span class="text-gray-800">{{ post.user.name }}</span>
+                    <small class="ml-2 text-sm text-gray-600">{{ new Date(post.created_at).toLocaleString() }}</small>
+                </div>
+            </div>
+            <p class="mt-4 text-lg text-gray-900">{{ post.message }}</p>
+        </div>
+    </div>
+</template>
